8b2023b87dc361ea4334010c87be393ebb64725d
GiJungCho <wzxcv123@naver.com>
2023-02-27 18:58:20 +0900

    Add files via upload

--- binance_f/constant/test.py
+++ binance_f/constant/test.py
@@ -1,8 +1,8 @@
 import os
 if(os.path.exists("binance_f/privateconfig.py")):
     from binance_f.privateconfig import *
-    g_api_key = 'SE4VoeIa2ObXkxooN2NcYJhv0FYBgffmbsn9ODtqXtteY9gs2eX8L1adyYAyC8Im'
-    g_secret_key = '1MPys7009oUtxGO5BjLB9quBtsBK5Dk54ooVIRLARpTLTy2x0IxFZqcIewNvmMCn'
+    g_api_key = p_api_key
+    g_secret_key = p_secret_key
 else:
     g_api_key = "SE4VoeIa2ObXkxooN2NcYJhv0FYBgffmbsn9ODtqXtteY9gs2eX8L1adyYAyC8Im"
     g_secret_key = "1MPys7009oUtxGO5BjLB9quBtsBK5Dk54ooVIRLARpTLTy2x0IxFZqcIewNvmMCn"
